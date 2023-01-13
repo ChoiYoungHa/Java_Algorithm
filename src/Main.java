76b155dd613840263549f28d6b3934f21c867931
YoungHaChoi <askillofgod159@naver.com>
2023-01-13 14:23:27 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,44 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
         int n = sc.nextInt();
-
-        boolean[] solution = solution(m, n);
-        for (int i = m; i < n + 1; i++) {
-            if (solution[i] == false) {
-                System.out.println(i);
-            }
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
 
+        ArrayList<Integer> solution = solution(arr);
+        for (int x : solution) {
+            System.out.print(x + " ");
+        }
     }
 
-    public static boolean [] solution(int m, int n) {
+    public static ArrayList<Integer> solution(int [] m) {
 
-        boolean [] answer = new boolean[n + 1];
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < m.length; i++) {
+            int res = 0;
+            int temp = m[i];
+            while(temp > 0){
+                int t = temp % 10; // 끝값 추출
+                res = res * 10 + t; // 이해 안 됨 뒤집는거같긴한데 기억안남
+                temp = temp / 10; // temp가 0이될 때까지
+            }
+            if (isPrime(res)) list.add(res);
+        }
+        return list;
+    }
 
-        answer[0] = true;
-        answer[1] = true;
-
-        for (int i = 2; i < Math.sqrt(n + 1); i++) {
-            for (int j = i * i; j < n + 1; j = j + i) {
-                answer[j] = true;
+    public static boolean isPrime(int num) {
+        if (num == 1) return false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
             }
         }
-
-
-        return answer;
-
+        return true;
     }
 }
