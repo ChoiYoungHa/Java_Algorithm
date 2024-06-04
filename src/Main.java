f303f1f5f49e0fb4b32dd87da9f4e1729f1157c1
YoungHaChoi <askillofgod159@naver.com>
2024-06-04 20:49:41 +0900

    commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import ex01_string.ex_01;
+import ex01_string.ex_02;
+
+public class Main {
+    public static void main(String[] args) {
+        int result = ex_02.solution(-4, 7,true);
+        System.out.println("result = " + result);
+    }
+}
