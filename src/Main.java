21bf9646b19cec2ea5156f73151412262ade90b9
YoungHaChoi <askillofgod159@naver.com>
2024-06-06 17:10:52 +0900

    코드처리하기

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import ex01_string.ex_01;
 import ex01_string.ex_02;
+import ex01_string.ex_03;
 
 public class Main {
     public static void main(String[] args) {
-        int result = ex_02.solution(-4, 7,true);
-        System.out.println("result = " + result);
+        String ddd = ex_03.solution("abc1abc1abc");
+        System.out.println("result = " + ddd);
     }
 }
