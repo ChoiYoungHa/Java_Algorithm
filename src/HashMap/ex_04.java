dd5567393470451ade3a2128fca3702e2c0e5a16
YoungHaChoi <askillofgod159@naver.com>
2024-06-27 15:26:30 +0900

    commit

--- src/HashMap/ex_04.java
+++ src/HashMap/ex_04.java
@@ -10,19 +10,35 @@ public class ex_04 {
         String t = sc.nextLine();
 
         ex_04 ex = new ex_04();
-        ex.solution(s, t);
+        System.out.print(ex.solution(s, t));
     }
 
     public int solution(String s, String t) {
+        char[] charArray = s.toCharArray();
+        int lt = 0;
         int answer = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
-        char[] arry = s.toCharArray();
-        for (int i = 0; i < t.length(); i++) {
-            map.put(arry[i], map.getOrDefault(arry[i], 0) + 1);
-        }
-        // lt, rt 슬라이딩 윈도우로 bac, aca, caA 이런식으로 밀고 나가면서 요소가 바뀔 때마다 t와 키값 비교해서 하나라도 틀리면 false
-        // 맞으면 answer + 1; 하면 될듯? 채영이 만나러가자.
+        HashMap<Character, Integer> s_map = new HashMap<>();
+        HashMap<Character, Integer> t_map = new HashMap<>();
 
+        // 슬라이딩 윈도우 세팅 1개전까지
+        for (int i = 0; i < t.length() - 1; i++) {
+            s_map.put(charArray[i], s_map.getOrDefault(charArray[i], 0) + 1);
+        }
+
+        // 비교할 아나그램 hashmap에 넣기
+        for (char x : t.toCharArray()) {
+            t_map.put(x, t_map.getOrDefault(x, 0) + 1);
+        }
+
+        for (int rt = t.length() - 1; rt < s.length(); rt++) {
+            s_map.put(charArray[rt], s_map.getOrDefault(charArray[rt], 0) + 1);
+            if (s_map.equals(t_map)) answer += 1;
+
+            s_map.put(charArray[lt], s_map.get(charArray[lt]) - 1);
+            if (s_map.get(charArray[lt]) == 0) s_map.remove(charArray[lt]);
+            lt++;
+        }
         return answer;
+
     }
 }
